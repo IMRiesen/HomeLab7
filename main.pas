aa61ec8cc9dafebd033453e8ee65544654481be1
IMRiesen <79581574+IMRiesen@users.noreply.github.com>
2024-05-21 15:16:37 +0300

    Add files via upload

---
+++ main.pas
@@ -0,0 +1,28 @@
+﻿uses GraphWPF, module1;
+
+var
+  (u, p, wx, wy) := (32, 4, 64, 64);
+
+procedure KeyDown(k: Key);
+begin
+  case K of
+    Key.Down: wy -= 10;
+    Key.Up: wy += 10; 
+    Key.Left: wx += 10;  
+    Key.Right: wx -= 10;
+    Key.Escape: halt(1); // выход
+    Key.A: if p * u < 1000 then u := (p * u > 50) ? (u * 1.1).trunc : u + 5;
+    Key.Z: if (p * u > 50) and (u > 9) then u := (u / 1.1).trunc;
+    Key.S: if (p * u > 50) and (p < 5) then (u, p) := (u div 2, p + 1);
+    Key.X: if p > 1 then (u, p) := (u * 2, p - 1);
+  end;
+  MoveTo(wx, wy);
+  Redraw(() -> begin Window.Clear; a(p); end); 
+end;
+
+begin
+  Window.SetSize(650, 650);
+  MoveTo(wx, wy);
+  Redraw(() -> a(p));
+  OnKeyDown += KeyDown;
+end.
---
+++ module1.pas
@@ -0,0 +1,60 @@
+﻿unit module1;
+
+interface
+
+procedure a(i: Integer);
+procedure b(i: Integer);
+procedure c(i: Integer);
+procedure d(i: Integer);
+
+implementation
+
+uses GraphWPF;
+
+var (u, p, wx, wy) := (32, 4, 64, 64);
+
+procedure a(i: Integer);
+begin
+  if i > 0 then
+  begin
+    d(i - 1); LineBy(+u, 0);
+    a(i - 1); LineBy(0, u);
+    a(i - 1); LineBy(-u, 0);
+    c(i - 1);
+  end;
+end;
+
+procedure b(i: Integer);
+begin
+  if i > 0 then
+  begin
+    c(i - 1); LineBy(-u, 0);
+    b(i - 1); LineBy(0, -u);
+    b(i - 1); LineBy(u, 0);
+    d(i - 1);
+  end;
+end;
+
+procedure c(i: Integer);
+begin
+  if i > 0 then
+  begin
+    b(i - 1); LineBy(0, -u);
+    c(i - 1); LineBy(-u, 0);
+    c(i - 1); LineBy(0, u);
+    a(i - 1);
+  end;
+end;
+
+procedure d(i: Integer);
+begin
+  if i > 0 then
+  begin
+    a(i - 1); LineBy(0, u);
+    d(i - 1); LineBy(u, 0);
+    d(i - 1); LineBy(0, -u);
+    b(i - 1);
+  end;
+end;
+
+end.
